82cc5e0913ff5b0d72b0654ea0e1be59f6a71c9f
Anton Kuznetsov <anton66656@yandex.ru>
2025-11-27 20:10:15 +0100

    Commit 3

--- exel_list.py
+++ exel_list.py
@@ -1,10 +1,12 @@
 import gspread # Импортируем gspread — библиотека для работы с Google Sheets
 from google.oauth2.service_account import Credentials #Импортируем класс Credentials из Google API Используется для авторизации через файл credentials.json, который ты скачал при создании сервисного аккаунта.
 from datetime import datetime
+import os, json
 
 
 scopes = ['https://www.googleapis.com/auth/spreadsheets'] #Указываем область доступа (scopes) Это означает: «даю доступ к Google Sheets полностью».
-creds = Credentials.from_service_account_file('credentials.json', scopes=scopes) #Загружаем credentials.json и создаём объект creds
+creds = Credentials.from_service_account_file('/etc/secrets/credentials.json', scopes=scopes) #Загружаем credentials.json и создаём объект creds
+
 client = gspread.authorize(creds) #Авторизуемся в gspread
 sheet_id = '1UmZ6CD6xN5Rbt9DaEE2vZEqM1QTL0gjghEAiCo4ac-A' #Указываем ID твоей таблицы
 workbook = client.open_by_key(sheet_id) #Открываем саму таблицу
